6ba1c854474f87278fbd80d6ac1dc25ccb44777c
InternetBugs <77239633+InternetBugs@users.noreply.github.com>
2024-04-22 02:07:48 +0800

    Update index.vue
    Signed-off-by: InternetBugs <77239633+InternetBugs@users.noreply.github.com>

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -56,7 +56,7 @@ const currentChatModel = computed(() => nowSelectChatModel.value ?? currentChatH
 
 const currentNavIndexRef = ref<number>(-1)
 
-const isVisionModel = computed(() => currentChatModel.value && (currentChatModel.value.includes('vision') || currentChatModel.value.includes('claude-3') || currentChatModel.value.includes('gpt-4-turbo')))
+const isVisionModel = computed(() => currentChatModel.value && (currentChatModel.value.includes('vision') || currentChatModel.value.includes('claude-3') || currentChatModel.value.includes('coze')))
 
 let loadingms: MessageReactive
 let allmsg: MessageReactive
